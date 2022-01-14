ec6f53603e952aba9a05295c092d9b7d51cd43cc
Pandademic <74566464+Pandademic@users.noreply.github.com>
2022-01-13 20:22:30 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video="";
-status=""
+status="";
+objects=[];
 function preload(){
     video=createVideo('video.mp4');
     video.hide();
@@ -11,6 +12,19 @@ function setup(){
 }
 function draw(){
     image(video,0,0,480,380);
+    if(status != ""){
+        objectDetector.detect(video,gotResult());
+        for(i=0;i < objects.length ; i++){
+            document.getElementById("status").innerHTML = "status: Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Count of objects detected: " + objects.length ;
+            fill("red");
+            percent=floor(objects[i].confidence * 100);
+            text(objects[i].label + "  " + percent + "%",objects[i].x + 15,objects[i].y + 15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height );
+        }
+    }
 }
 function start(){
     objectDetector=ml5.objectDetector('cocossd',modelLoaded);
@@ -23,4 +37,13 @@ function modelLoaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+function gotResult(error,results){
+    
+    if(error){
+        console.log(error);
+    }
+    
+    console.log(results);
+    objects = results ;
 }
